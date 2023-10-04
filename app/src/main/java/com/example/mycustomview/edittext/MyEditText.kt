c761f1fac7481696d5fb72faadf275141bdadd51
LoyaltyDev11 <arifsatria5554@gmail.com>
2023-10-04 18:17:27 +0700

    git commit editText

--- app/src/main/java/com/example/mycustomview/edittext/MyEditText.kt
+++ app/src/main/java/com/example/mycustomview/edittext/MyEditText.kt
@@ -1,10 +1,15 @@
 package com.example.mycustomview.edittext
 
 import android.content.Context
+import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
 
 class MyEditText: AppCompatEditText{
     constructor(context: Context): super(context) {
+
+    }
+
+    constructor(context: Context,attr: AttributeSet): super(context,attr) {
 
     }
 }
