638bb085b92707b29bd4bb5a31f95503e63937d9
LoyaltyDev11 <arifsatria5554@gmail.com>
2023-10-04 18:13:17 +0700

    git commit editText

---
+++ app/src/main/java/com/example/mycustomview/edittext/MyEditText.kt
@@ -0,0 +1,10 @@
+package com.example.mycustomview.edittext
+
+import android.content.Context
+import androidx.appcompat.widget.AppCompatEditText
+
+class MyEditText: AppCompatEditText{
+    constructor(context: Context): super(context) {
+
+    }
+}
